68e7c4ee4c7b896f120c8ba4b2600ad5f36f3013
Jorge Lobo <jorgeavlobo@gmail.com>
2026-07-11 22:02:07 +0100

    Add project files.

---
+++ IntercomFirmwareTool.Core/Ext4Probe.cs
@@ -0,0 +1,19 @@
+﻿using SharpExt4;
+using System.Text;
+
+namespace IntercomFirmwareTool.Core
+{
+    public static class Ext4Probe
+    {
+        public static string ReadFile(string imagePath, string fileInsideImage)
+        {
+            var disk = ExtDisk.Open(imagePath);
+            var fs = ExtFileSystem.Open(disk.Partitions[0]);
+            var file = fs.OpenFile(fileInsideImage, FileMode.Open, FileAccess.Read);
+            var buf = new byte[file.Length];
+            file.Read(buf, 0, buf.Length);
+            file.Close();
+            return Encoding.UTF8.GetString(buf);
+        }
+    }
+}
